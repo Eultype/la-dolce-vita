ab74ea038d41775c264b19524ad9f6835427f538
Eultype <Sdarryy59@gmail.com>
2026-01-23 00:32:24 +0100

    refactor(data): extraction des données du menu dans un fichier constant dédié

---
+++ src/data/menu.ts
@@ -0,0 +1,23 @@
+export const dishes = [
+    {
+        name: "Pizza Margherita",
+        description: "L'essence de la simplicité : Mozzarella di Bufala, tomates San Marzano et basilic frais.",
+        price: "16",
+        image: "/images/pizza-dish.jpg",
+        origin: "NAPOLI"
+    },
+    {
+        name: "Carbonara Romana",
+        description: "Le chef d'œuvre du Latium : Guanciale croustillant, Pecorino Romano DOP et œufs de ferme.",
+        price: "18",
+        image: "/images/pasta-dish.jpg",
+        origin: "ROMA"
+    },
+    {
+        name: "Tiramisù Classico",
+        description: "Un nuage de douceur : Mascarpone onctueux, café espresso et biscuits artisanaux.",
+        price: "09",
+        image: "/images/tiramisu-dish.jpg",
+        origin: "VENEZIA"
+    },
+];
